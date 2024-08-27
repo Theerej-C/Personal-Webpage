0167004d505b4296cf25c99b41248e69601a7d67
Theerej <theerej@gmail.com>
2024-08-27 07:36:41 +0530

    User model modification
    Modified user model due to id field requirement and also rewrote the serializer

--- backend/django-backend/personal_backend/user/models.py
+++ backend/django-backend/personal_backend/user/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
-class CustomUser(AbstractUser):
+class AbstractMyUser(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255,unique=True)
     phone_number = models.CharField(max_length=20)
-    
+    USERNAME_FIELD = 'username'  # or 'username' if you prefer
+    REQUIRED_FIELDS = ['email'] 
+    class Meta:
+        abstract = True
+class CustomUser(AbstractMyUser):
+    pass    
--- backend/django-backend/personal_backend/user/serializers.py
+++ backend/django-backend/personal_backend/user/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'first_name','last_name','phone_number','password','is_active']
+        fields = ['id','username','email','password','is_active','phone_number']
     def create(self,validated_data):
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
