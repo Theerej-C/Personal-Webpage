bd7dc480b36d55f3143f2cb7c07d137a34fb1ff9
Theerej-C <theerej@gmail.com>
2024-09-07 10:09:29 +0530

    adding style to register page

--- frontend/frontend-nextjs/src/app/register/page.tsx
+++ frontend/frontend-nextjs/src/app/register/page.tsx
@@ -8,9 +8,11 @@ interface RegisterPageProps {
 
 const RegisterPage: React.FC<RegisterPageProps> = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <h1>Register Page</h1>
       <RegisterForm />
+    </div>
     </div>
   );
 };
